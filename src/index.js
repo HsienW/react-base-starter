c8e3ae445eba58c92a2cf2c8acc71c3ab96b95e0
10704302 <Wa@777888999>
2021-10-16 11:54:34 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
-// import {Main} from './containers/main';
-// import {IntlProvider} from 'react-intl';
-// import {LOCALES} from './locale/locale';
-// import {messages} from './locale/mesage';
-import {TestForm} from './containers/form';
-
-// const locale = LOCALES.JAPANESE;
-
-// render(
-//     <IntlProvider
-//         messages={messages[locale]}
-//         locale={locale}
-//         defaultLocale={LOCALES.ENGLISH}
-//     >
-//         <testForm/>
-//     </IntlProvider>, document.getElementById('app')
-// );
+import {Main} from './containers/main';
 
 render(
-    <TestForm/>, document.getElementById('app')
+    <Main/>, document.getElementById('app')
 );
--- src/locale/locale.js
+++
@@ -1,6 +0,0 @@
-export const LOCALES = {
-    ENGLISH: 'en-US',
-    JAPANESE: 'ja-JA',
-    FRENCH: 'fr-FR',
-    GERMAN: 'de-DE',
-};
